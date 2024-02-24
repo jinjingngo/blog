9615d70fb1267a02bc68030c828f65eb9ee82568
Jinjing Wu <jinjing.wu@compass.com>
2024-02-24 22:32:56 +0100

    feat: new portfolio build in Next.js

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Jinjing Wu",
+  icons: "./favicon.png",
+  authors: {
+    url: "https://wujinjing.com",
+    name: "Jinjing Wu",
+  },
+  description: "Jinjing Wu's portfolio",
+  keywords:
+    "Portfolio,FullStack Developer,Frontend Developer,Backend Developer",
+  // TODO: Twitter metadata
+  // TODO: OpenGraph metadata
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
