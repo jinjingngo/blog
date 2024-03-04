cccdc4594ab889e0b14f23ead2cc5acc6c1f8fa7
Jinjing Wu <jinjing.wu@compass.com>
2024-03-04 21:34:55 +0100

    feat: more pages content comes later

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,7 @@
+export default function Resume() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1>Resume</h1>
+    </main>
+  );
+}
---
+++ src/app/services/page.tsx
@@ -0,0 +1,7 @@
+export default function Services() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1>Services</h1>
+    </main>
+  );
+}
