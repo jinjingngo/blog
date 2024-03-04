3559cc1904a499f7a09186c3298f75421a403c34
Jinjing Wu <wujinjing25@gmail.com>
2024-03-04 23:33:22 +0100

    feat: resume page (#12)
    * feat: resume page
    
    * fix: pdf file url issue
    
    * fix: pdf 404 issue with native <a> tag and add small spicy

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,7 +1,24 @@
+import classes from "./resume.module.css";
+import ResumeCard from "../components/ResumeCard";
+import type { ResumeCardProps } from "../components/ResumeCard";
+
+const resumes: ResumeCardProps[] = [
+  {
+    position: "Senior Frontend Engineer",
+    url: "/resumes/Jinjing Wu_Senior Frontend Developer_CV.pdf",
+  },
+  {
+    position: "Senior Full-Stack Engineer",
+    url: "/resumes/Jinjing Wu_Senior Full-Stack Developer_CV.pdf",
+  },
+];
+
 export default function Resume() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>Resume</h1>
+    <main className="min-main-h grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 gap-4 place-items-center p-24 md:px-40 text-lg md:text-xl">
+      {resumes.map((resume, index) => (
+        <ResumeCard key={index} {...resume}></ResumeCard>
+      ))}
     </main>
   );
 }
