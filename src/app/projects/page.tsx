c573596f299c2a10075974d432c048850ac9e9eb
Jinjing Wu <jinjing.wu@compass.com>
2024-03-05 22:09:14 +0100

    polish the style

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,22 +4,10 @@ import type { ProjectCardProps } from "../components/ProjectCard";
 
 const projects: ProjectCardProps[] = [
   {
-    name: "Real Estate Panorama",
+    name: "CRM",
     description:
-      "A panorama generation system utilizing krpano, dramatically improving online property showcases for multiple real estate companies, boosting their market reach and customer interaction.",
-    poster: "/images/projects/real_estate_panorama.png",
-  },
-  {
-    name: "GIS Map",
-    description:
-      "A versatile GIS Map component, which became a core feature integrated across multiple products, elevating user experience and expanding product functionality",
-    poster: "/images/projects/gis_map_component.jpg",
-  },
-  {
-    name: "MLS Panels",
-    description:
-      "MLS Panel project, utilizing React for dynamic user interface design and Express to develop a robust GraphQL service, enhancing data integration and user experience.",
-    poster: "/images/projects/mls_filters.gif",
+      "Decomposed the main system and CRM system by consuming Kafka events to synchronize data in real-time, enhancing data reliability and system interoperability.",
+    poster: "/images/projects/crm.png",
     url: "https://www.compass.com/",
   },
   {
@@ -30,20 +18,38 @@ const projects: ProjectCardProps[] = [
     url: "https://www.compass.com/",
   },
   {
-    name: "CRM",
+    name: "MLS Panels",
     description:
-      "Decomposed the main system and CRM system by consuming Kafka events to synchronize data in real-time, enhancing data reliability and system interoperability.",
-    poster: "/images/projects/crm.png",
+      "MLS Panel project, utilizing React for dynamic user interface design and Express to develop a robust GraphQL service, enhancing data integration and user experience.",
+    poster: "/images/projects/mls_filters.gif",
     url: "https://www.compass.com/",
+  },
+  {
+    name: "GIS Map",
+    description:
+      "A versatile GIS Map component, which became a core feature integrated across multiple products, elevating user experience and expanding product functionality",
+    poster: "/images/projects/gis_map_component.jpg",
+  },
+  {
+    name: "Real Estate Panorama",
+    description:
+      "A panorama generation system utilizing krpano, dramatically improving online property showcases for multiple real estate companies, boosting their market reach and customer interaction.",
+    poster: "/images/projects/real_estate_panorama.png",
   },
 ];
 
 export default function Projects() {
   return (
     <main className={`min-main-h ${classes.projects}`}>
-      {projects.map((project, index) => (
-        <ProjectCard key={index} {...project} />
-      ))}
+      <p className={classes.projects__tip}>
+        The images serve merely as conceptual illustrations and do not represent
+        the actual work.
+      </p>
+      <section className={classes.projects__scroll_view}>
+        {projects.map((project, index) => (
+          <ProjectCard key={index} {...project} />
+        ))}
+      </section>
     </main>
   );
 }
