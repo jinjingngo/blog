fbc2a15a714c1c63ded2bc3b92c8a17a42a91c25
Jinjing Wu <wujinjing25@gmail.com>
2024-03-05 09:40:38 +0100

    feat: page contact (#18)

---
+++ src/app/components/ContactCard/index.ts
@@ -0,0 +1,4 @@
+import ContactCard from "./ContactCard";
+export default ContactCard;
+
+export * from "./ContactCard";
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,44 @@
+import classes from "./contact.module.css";
+import { FaGithub } from "react-icons/fa6";
+import { MdAlternateEmail } from "react-icons/md";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { FaXTwitter } from "react-icons/fa6";
+
+import ContactCard from "../components/ContactCard";
+
+import type { ContactCardProps } from "../components/ContactCard";
+
+const className = "w-8 h-8 md:w-12 md:h-12";
+
+const contacts: ContactCardProps[] = [
+  {
+    children: <MdAlternateEmail className={className} />,
+    url: "mailto: jinjing.ngo@gmail.com",
+    title: "Mail me",
+  },
+  {
+    children: <FaGithub className={className} />,
+    url: "https://github.com/charleserious",
+    title: "GitHub Profile",
+  },
+  {
+    children: <AiOutlineLinkedin className={className} />,
+    url: "https://www.linkedin.com/in/jinjingwu/",
+    title: "LinkedIn Profile",
+  },
+  {
+    children: <FaXTwitter className={className} />,
+    url: "https://twitter.com/JinjingNgo",
+    title: "X Profile",
+  },
+];
+
 export default function Contact() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>Contact Me</h1>
+    <main className="flex min-main-h flex-row flex-wrap gap-3 md:gap-4 items-center justify-center p-24">
+      {contacts.map((contact, index) => (
+        <ContactCard key={index} {...contact} />
+      ))}
     </main>
   );
 }
