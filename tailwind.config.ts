aaefbae1192d3fb1f5454ee11695e55cf2d56bc5
Jinjing Wu <jinjing.wu@compass.com>
2024-03-11 20:48:24 +0100

    feat: make the `HIRE ME` glow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,31 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        glow: {
+          from: {
+            "text-shadow": `0 0 10px white,
+                            0 0 20px white,
+                            0 0 30px salmon,
+                            0 0 40px salmon,
+                            0 0 50px salmon,
+                            0 0 60px salmon,
+                            0 0 70px salmon;`,
+          },
+          to: {
+            "text-shadow": `0 0 20px white,
+                            0 0 30px salmon,
+                            0 0 40px salmon,
+                            0 0 50px salmon,
+                            0 0 60px salmon,
+                            0 0 70px salmon,
+                            0 0 80px salmon;`,
+          },
+        },
+      },
+      animation: {
+        glow: "glow 1s ease-in-out infinite alternate",
+      },
     },
   },
   plugins: [],
