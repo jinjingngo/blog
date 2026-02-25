fef2617a18c313d4085e204096ca8d700260b9fe
Jinjing Wu <jinjing@nolimitcity.com>
2026-02-25 21:20:11 +0100

    chore: exlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "eslint/config";
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextOnPages from "eslint-plugin-next-on-pages";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
@@ -17,14 +16,9 @@ const compat = new FlatCompat({
 export default defineConfig([{
     extends: [
         ...nextCoreWebVitals,
-        ...compat.extends("plugin:next-on-pages/recommended")
     ],
 
-    plugins: {
-        "next-on-pages": nextOnPages,
-    },
+    plugins: {},
 
-    rules: {
-        "next-on-pages/no-unsupported-configs": "warn",
-    },
+    rules: {},
 }]);
