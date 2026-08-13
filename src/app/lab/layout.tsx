136c12cf2fa87dfd8ac165f687f277cbd6ae8452
Jinjing Wu <jinjing.ngo@gmail.com>
2026-08-13 21:38:50 +0200

    fix: address PR comment

--- src/app/lab/layout.tsx
+++ src/app/lab/layout.tsx
@@ -2,7 +2,7 @@ export default function LabLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<section className="flex min-h-full w-full max-w-4xl flex-col px-6 py-8 md:px-10 md:py-12">
+		<section className="flex w-full max-w-4xl flex-1 flex-col px-6 py-8 md:px-10 md:py-12">
 			{children}
 		</section>
 	);
